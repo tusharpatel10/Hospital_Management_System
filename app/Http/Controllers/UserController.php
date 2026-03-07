021e9ec7f97795d07e9cc19048f5ec803b5597ed
Tushar <tusharpatel1093@gmail.com>
2026-03-07 18:44:15 +0530

    Make the User controller, Route and divide the User Panel and Admin Panel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function Dashboard()
+    {
+        if (Auth::check() && Auth::user()->user_type == 'user') {
+            return view('dashboard');
+        } elseif (Auth::check() && Auth::user()->user_type == 'admin') {
+            return view('admin.dashboard');
+        } else {
+            return redirect('/');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'Dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +16,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
